7e8b11e441e730b771d3f1c0cbe34d63324db515
yeyoung <nimoisabella@gmail.com>
2020-08-09 23:36:57 +0900

    초기 커밋.

---
+++ library_practice/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace library_practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.id_text = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pw_text = new System.Windows.Forms.TextBox();
+            this.login_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // id_text
+            // 
+            this.id_text.ForeColor = System.Drawing.SystemColors.Window;
+            this.id_text.Location = new System.Drawing.Point(129, 84);
+            this.id_text.Name = "id_text";
+            this.id_text.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.id_text.Size = new System.Drawing.Size(100, 25);
+            this.id_text.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PW";
+            // 
+            // pw_text
+            // 
+            this.pw_text.Location = new System.Drawing.Point(129, 135);
+            this.pw_text.Name = "pw_text";
+            this.pw_text.Size = new System.Drawing.Size(100, 25);
+            this.pw_text.TabIndex = 3;
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(336, 87);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(123, 73);
+            this.login_button.TabIndex = 4;
+            this.login_button.Text = "Log in";
+            this.login_button.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "This is log in form";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.pw_text);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.id_text);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Login_form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox id_text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pw_text;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ library_practice/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace library_practice
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
